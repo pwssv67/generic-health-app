50ccf04d4f71d9d4d44075a2876d4383de353603
pwssv67 <61920177+pwssv67@users.noreply.github.com>
2020-10-18 01:34:55 +0300

    Started adding history charts instead of stupid recylerview with awful viewholder

--- app/src/main/java/ru/pwssv67/healthcounter/HistoryActivity.kt
+++ app/src/main/java/ru/pwssv67/healthcounter/HistoryActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_history_charts.*
 import ru.pwssv67.healthcounter.Adapters.HistoryAdapter
 import ru.pwssv67.healthcounter.Extensions.DayStats
 
@@ -12,15 +13,19 @@ class HistoryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history)
-        val recyclerView: RecyclerView = findViewById(R.id.rv_history)
+        setContentView(R.layout.activity_history_charts)
+        //val recyclerView: RecyclerView = findViewById(R.id.rv_history)
         val historyViewModel = HistoryViewModel(application)
         val adapter:HistoryAdapter
-        recyclerView.adapter = HistoryAdapter()
-        adapter = recyclerView.adapter as HistoryAdapter
+        val chart = chart_glasses
+       // recyclerView.adapter = HistoryAdapter()
+        //adapter = recyclerView.adapter as HistoryAdapter
         val observer = Observer<List<DayStats>> {
             if (it!=null) {
-                adapter.updateData(it)
+                val map = it.associateBy({it.day.drop(5)}, {it.glasses.toFloat()}).toMutableMap()
+                val lmap = LinkedHashMap(map)
+                chart.animate(lmap)
+                //adapter.updateData(it)
                 //days.reverse()
             }
         }
